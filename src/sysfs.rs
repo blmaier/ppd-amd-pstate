c6ccbf2756cf7987d155a888419f3b0c248eba31
Brandon Maier <brandon.maier@gmail.com>
2023-08-25 14:10:27 -0500

    add wrappers for common sysfs reading idioms

--- src/sysfs.rs
+++ src/sysfs.rs
@@ -1,19 +1,26 @@
+use std::error::Error;
 use std::fs;
 
+fn sysfs_read(path: &str) -> Result<String, Box<dyn Error>> {
+    Ok(String::from(fs::read_to_string(path)?.trim()))
+}
+
+macro_rules! sysfs_read {
+    ($($tts:tt)*) => {
+        sysfs_read(format!($($tts)*).as_str())
+    }
+}
+
 pub fn amd_pstate_is_active() -> bool {
-    match fs::read_to_string("/sys/devices/system/cpu/amd_pstate/status") {
-        Ok(s) => s.trim() == "active",
+    match sysfs_read("/sys/devices/system/cpu/amd_pstate/status") {
+        Ok(s) => s == "active",
         Err(_) => false,
     }
 }
 
 pub fn cpux_scaling_driver(cpu: usize) -> String {
-    let path = format!("/sys/devices/system/cpu/cpu{}/cpufreq/scaling_driver", cpu);
-    String::from(
-        fs::read_to_string(path.as_str())
-            .expect("Failed to read active scaling driver")
-            .trim(),
-    )
+    sysfs_read!("/sys/devices/system/cpu/cpu{}/cpufreq/scaling_driver", cpu)
+        .expect("Failed to read active scaling driver")
 }
 
 pub fn cpux_scaling_driver_is_epp(cpu: usize) -> bool {
@@ -45,60 +52,44 @@ pub fn cpu_parse_range(cpu_string: &str) -> Vec<usize> {
 }
 
 pub fn cpu_possible() -> Vec<usize> {
-    let present =
-        fs::read_to_string("/sys/devices/system/cpu/possible").expect("Failed to read CPU present");
-    cpu_parse_range(present.trim())
+    let possible =
+        sysfs_read("/sys/devices/system/cpu/possible").expect("Failed to read CPU present");
+    cpu_parse_range(possible.as_str())
 }
 
 pub fn cpux_scaling_governor_active(cpu: usize) -> String {
-    let path = format!(
+    sysfs_read!(
         "/sys/devices/system/cpu/cpu{}/cpufreq/scaling_governor",
         cpu
-    );
-    String::from(
-        fs::read_to_string(path.as_str())
-            .expect("Failed to read active scaling governor")
-            .trim(),
     )
+    .expect("Failed to read active scaling governor")
 }
 
 pub fn cpux_scaling_governor_avail(cpu: usize) -> Vec<String> {
-    let path = format!(
+    let avail = sysfs_read!(
         "/sys/devices/system/cpu/cpu{}/cpufreq/scaling_available_governors",
         cpu
-    );
-    let avail_str = String::from(
-        fs::read_to_string(path.as_str())
-            .expect("Failed to read available scaling governors")
-            .trim(),
-    );
-    let avail = avail_str.split_whitespace();
+    )
+    .expect("Failed to read available scaling governors");
+    let avail = avail.split_whitespace();
     avail.map(|x| String::from(x)).collect()
 }
 
 pub fn cpux_epp_active(cpu: usize) -> String {
-    let path = format!(
+    sysfs_read!(
         "/sys/devices/system/cpu/cpu{}/cpufreq/energy_performance_preference",
         cpu
-    );
-    String::from(
-        fs::read_to_string(path.as_str())
-            .expect("Failed to read EPP active")
-            .trim(),
     )
+    .expect("Failed to read EPP active")
 }
 
 pub fn cpux_epp_avail(cpu: usize) -> Vec<String> {
-    let path = format!(
+    let avail = sysfs_read!(
         "/sys/devices/system/cpu/cpu{}/cpufreq/energy_performance_available_preferences",
         cpu
-    );
-    let avail_str = String::from(
-        fs::read_to_string(path.as_str())
-            .expect("Failed to read EPP available")
-            .trim(),
-    );
-    let avail = avail_str.split_whitespace();
+    )
+    .expect("Failed to read EPP available");
+    let avail = avail.split_whitespace();
     avail.map(|x| String::from(x)).collect()
 }
 
