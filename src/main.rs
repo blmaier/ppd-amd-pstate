0969915f2b613000f3b25620b381c768139fc29d
Brandon Maier <brandon.maier@gmail.com>
2023-08-25 22:16:20 -0500

    propagate errors from sysfs

--- src/main.rs
+++ src/main.rs
@@ -15,27 +15,42 @@ fn power_profile_active() -> String {
     p.active_profile().expect("get active profile error")
 }
 
+fn errstr<V: std::fmt::Display, E: std::fmt::Display>(res: Result<V, E>) -> String {
+    res.map_or_else(|e| e.to_string(), |v| v.to_string())
+}
+
 fn print_info() {
     println!("amd pstate status: {}", sysfs::amd_pstate_is_active());
     println!("Power Profile: {}", power_profile_active());
-    for cpu in sysfs::cpu_possible() {
-        println!("cpu{}", cpu);
-        println!("  scaling driver: {}", sysfs::cpux_scaling_driver(cpu));
-        println!(
-            "  scaling driver is epp: {}",
-            sysfs::cpux_scaling_driver_is_epp(cpu)
-        );
-        println!("  epp active: {}", sysfs::cpux_epp_active(cpu));
-        println!("  epp avail: {}", sysfs::cpux_epp_avail(cpu).join(", "));
-        println!(
-            "  scaling governor active: {}",
-            sysfs::cpux_scaling_governor_active(cpu)
-        );
-        println!(
-            "  scaling governor avail: {}",
-            sysfs::cpux_scaling_governor_avail(cpu).join(", ")
-        );
-        break;
+    match sysfs::cpu_possible() {
+        Ok(cpus) => {
+            for cpu in cpus.into_iter() {
+                println!("cpu{}", cpu);
+                println!(
+                    "  scaling driver: {}",
+                    errstr(sysfs::cpux_scaling_driver(cpu))
+                );
+                println!(
+                    "  scaling driver is epp: {}",
+                    sysfs::cpux_scaling_driver_is_epp(cpu)
+                );
+                println!("  epp active: {}", errstr(sysfs::cpux_epp_active(cpu)));
+                println!(
+                    "  epp avail: {}",
+                    errstr(sysfs::cpux_epp_avail(cpu).map(|e| e.join(", ")))
+                );
+                println!(
+                    "  scaling governor active: {}",
+                    errstr(sysfs::cpux_scaling_governor_active(cpu))
+                );
+                println!(
+                    "  scaling governor avail: {}",
+                    errstr(sysfs::cpux_scaling_governor_avail(cpu).map(|e| e.join(", ")))
+                );
+                break;
+            }
+        }
+        Err(e) => println!("No CPUs found: {:#?}", e),
     }
 }
 
@@ -43,8 +58,8 @@ fn assert_amd_pstate() {
     if !sysfs::amd_pstate_is_active() {
         panic!("System is not using AMD pstate");
     };
-    for cpu in sysfs::cpu_possible() {
-        if sysfs::cpux_scaling_driver(cpu) != "amd-pstate-epp" {
+    for cpu in sysfs::cpu_possible().expect("No CPUs found") {
+        if sysfs::cpux_scaling_driver(cpu).expect("No scaling driver") != "amd-pstate-epp" {
             panic!("cpu{} not using amd-pstate-epp scaling driver", cpu);
         };
     }
--- src/sysfs.rs
+++ src/sysfs.rs
@@ -1,7 +1,8 @@
-use std::error::Error;
 use std::fs;
+use std::io;
+use std::error::Error;
 
-fn sysfs_read(path: &str) -> Result<String, Box<dyn Error>> {
+fn sysfs_read(path: &str) -> io::Result<String> {
     Ok(String::from(fs::read_to_string(path)?.trim()))
 }
 
@@ -18,79 +19,71 @@ pub fn amd_pstate_is_active() -> bool {
     }
 }
 
-pub fn cpux_scaling_driver(cpu: usize) -> String {
+pub fn cpux_scaling_driver(cpu: usize) -> io::Result<String> {
     sysfs_read!("/sys/devices/system/cpu/cpu{}/cpufreq/scaling_driver", cpu)
-        .expect("Failed to read active scaling driver")
 }
 
 pub fn cpux_scaling_driver_is_epp(cpu: usize) -> bool {
-    cpux_scaling_driver(cpu) == "amd-pstate-epp"
+    match cpux_scaling_driver(cpu) {
+        Ok(s) => s == "amd-pstate-epp",
+        Err(_) => false,
+    }
 }
 
-pub fn cpu_parse_range(cpu_string: &str) -> Vec<usize> {
+pub fn cpu_parse_range(cpu_string: &str) -> Result<Vec<usize>, Box<dyn Error>> {
+    use std::ops::RangeInclusive;
+
     let groups = cpu_string.split(",");
 
-    groups
-        .map(|group| {
-            let mut range = group.split("-");
+    let range_maps = groups.map(|group| {
+        let mut range = group.split("-");
 
-            let left = range
-                .next()
-                .expect("CPU possible contains invalid range")
-                .parse::<usize>()
-                .expect("CPU possible contains invalid left range");
-            let right = match range.next() {
-                Some(x) => x
-                    .parse::<usize>()
-                    .expect("CPU possible contains invalid right range"),
-                None => left,
-            };
-            left..=right
-        })
-        .flatten()
-        .collect()
+        let left = range.next().ok_or("?")?.parse::<usize>()?;
+        let right = match range.next() {
+            Some(x) => x.parse::<usize>()?,
+            None => left,
+        };
+        Ok(left..=right)
+    });
+    let ranges: Result<Vec<RangeInclusive<usize>>, Box<dyn Error>> = range_maps.collect();
+    Ok(ranges?.into_iter().flatten().collect())
 }
 
-pub fn cpu_possible() -> Vec<usize> {
-    let possible =
-        sysfs_read("/sys/devices/system/cpu/possible").expect("Failed to read CPU present");
+pub fn cpu_possible() -> Result<Vec<usize>, Box<dyn Error>> {
+    let possible = sysfs_read("/sys/devices/system/cpu/possible")?;
     cpu_parse_range(possible.as_str())
 }
 
-pub fn cpux_scaling_governor_active(cpu: usize) -> String {
+pub fn cpux_scaling_governor_active(cpu: usize) -> io::Result<String> {
     sysfs_read!(
         "/sys/devices/system/cpu/cpu{}/cpufreq/scaling_governor",
         cpu
     )
-    .expect("Failed to read active scaling governor")
 }
 
-pub fn cpux_scaling_governor_avail(cpu: usize) -> Vec<String> {
+pub fn cpux_scaling_governor_avail(cpu: usize) -> io::Result<Vec<String>> {
     let avail = sysfs_read!(
         "/sys/devices/system/cpu/cpu{}/cpufreq/scaling_available_governors",
         cpu
-    )
-    .expect("Failed to read available scaling governors");
+    )?;
     let avail = avail.split_whitespace();
-    avail.map(|x| String::from(x)).collect()
+    Ok(avail.map(|x| String::from(x)).collect())
 }
 
-pub fn cpux_epp_active(cpu: usize) -> String {
+pub fn cpux_epp_active(cpu: usize) -> io::Result<String> {
     sysfs_read!(
         "/sys/devices/system/cpu/cpu{}/cpufreq/energy_performance_preference",
         cpu
     )
-    .expect("Failed to read EPP active")
 }
 
-pub fn cpux_epp_avail(cpu: usize) -> Vec<String> {
+pub fn cpux_epp_avail(cpu: usize) -> io::Result<Vec<String>> {
     let avail = sysfs_read!(
         "/sys/devices/system/cpu/cpu{}/cpufreq/energy_performance_available_preferences",
         cpu
-    )
-    .expect("Failed to read EPP available");
+    )?;
     let avail = avail.split_whitespace();
-    avail.map(|x| String::from(x)).collect()
+    Ok(avail.map(|x| String::from(x)).collect())
 }
 
 #[cfg(test)]
@@ -98,21 +91,22 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_cpu_parse_range() {
-        assert_eq!(cpu_parse_range("0-4"), (0..=4).collect::<Vec<usize>>());
+    fn test_cpu_parse_range() -> Result<(), Box<dyn Error>> {
+        assert_eq!(cpu_parse_range("0-4")?, (0..=4).collect::<Vec<usize>>());
         assert_eq!(
-            cpu_parse_range("0-4,6"),
+            cpu_parse_range("0-4,6")?,
             (0..=4).chain(6..=6).collect::<Vec<usize>>()
         );
         assert_eq!(
-            cpu_parse_range("0-4,6-8"),
+            cpu_parse_range("0-4,6-8")?,
             (0..=4).chain(6..=8).collect::<Vec<usize>>()
         );
-        assert_eq!(cpu_parse_range("1-4"), (1..=4).collect::<Vec<usize>>());
+        assert_eq!(cpu_parse_range("1-4")?, (1..=4).collect::<Vec<usize>>());
         assert_eq!(
-            cpu_parse_range("1-1000"),
+            cpu_parse_range("1-1000")?,
             (1..=1000).collect::<Vec<usize>>()
         );
-        assert_ne!(cpu_parse_range("0-3"), (0..=4).collect::<Vec<usize>>());
+        assert_ne!(cpu_parse_range("0-3")?, (0..=4).collect::<Vec<usize>>());
+        Ok(())
     }
 }
